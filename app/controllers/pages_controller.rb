090bedd268af7924854b65c2cc72b7482468726f
amine el chattahi <amine.e@chrix.ma>
2026-04-07 19:07:51 +0100

    modifications mineures  home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :about]
 
   def home
     @current_user = current_user if @user_signed_in
