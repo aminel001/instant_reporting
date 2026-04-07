5228369817d52b375163c28dbfee9628b9ca10fd
amine el chattahi <amine.e@chrix.ma>
2026-04-07 19:48:45 +0100

    main dashboard page created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :main]
 
   def home
     @current_user = current_user if @user_signed_in
   end
 
   def about
+  end
+
+  def main
   end
 end
