de6667629a398314ac0e3589d7e1d11f762a5e99
Zakarya KARTTI <kartti.zakarya@gmail.com>
2026-04-21 16:22:38 +0100

    add pdf export

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "html2canvas", to: "https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/+esm"
+pin "jspdf", to: "https://cdn.jsdelivr.net/npm/jspdf@2.5.1/+esm"
